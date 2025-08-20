b65957abe306544e16cc99d250cc818bc447cc7b
Bryan Garcia <bryan1997st@gmail.com>
2025-08-20 16:45:18 -0500

    Add Projects component: implement a new Projects section featuring a carousel of project cards, utilizing a utility file for project data and responsive design for improved user experience.

---
+++ src/components/Home/Projects/projects.utils.js
@@ -0,0 +1,16 @@
+
+export const projects = [
+    {
+        name: "Devcorner",
+        description:
+            "A blog sharing coding techniques, tools, and tips to help developers learn and grow.",
+        path: "/qr-code",
+        repo: "",
+        technologies: 'TypeScript | React | Strapi | Docker | NGINX | AWS',
+        website: 'https://www.devcorner.top/',
+        image: {
+            url: 'https://resourceboy.com/wp-content/uploads/2023/05/4-angles-of-macbook-mockups-on-the-table-1.jpg',
+            alt: 'Devcorner webpage'
+        },
+    }
+];
