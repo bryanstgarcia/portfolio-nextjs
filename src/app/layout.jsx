e3f31b1a640279a6f7f7e25663363ff8c1bc0518
Bryan Garcia <bryan1997st@gmail.com>
2025-08-20 18:17:45 -0500

    Add background image and update favicon; enhance layout metadata for improved SEO and social sharing with new keywords and descriptions.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,45 @@
-import "./globals.css";
+import './globals.css';
 
 import { Navbar } from '@app/components/shared/Navbar';
-import { outerSans } from "./layoutUtils";
+import { outerSans } from './layoutUtils';
 
 export const metadata = {
-  title: "Bryan Garcia | Full stack developer",
-  description: "Bryan Garcia Portfolio. Full stack ",
+  title: 'Bryan Garcia | Full stack developer',
+  description: 'Full stack developer. Python and React as main tools.',
+  keywords: ['Next.js', 'React', 'JavaScript', 'TypeScript', 'Python', 'Flask', 'Full stack'],
+  creator: 'Bryan García',
+  category: 'technology',
+  openGraph: {
+    title: 'Bryan Garcia | Full stack developer',
+    description: 'Full stack developer. Python and React as main tools.',
+    url: 'https://bryanstgarcia.com',
+    siteName: 'Bryan Garcia Portfolio',
+    images: 'https://bryanstgarcia.com/images/bg-image.png',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Bryan Garcia | Full stack developer',
+    description: 'Full stack developer. Python and React as main tools.',
+    creator: '@bryanstgarcia',
+    images: ['https://bryanstgarcia.com/images/bg-image.png'], // Must be an absolute URL
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${outerSans.variable} antialiased`}
       >
