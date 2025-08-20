2f3099246a21ffb1e5081659a8221f8d923b6c5c
Bryan Garcia <bryan1997st@gmail.com>
2025-08-20 16:47:58 -0500

    Add ProjectsCounter component: implement a new section to display project statistics, including projects completed, years of experience, and collaborations, with responsive design and associated styles.

---
+++ src/components/Home/ProjectsCounter/ProjectsCounter.jsx
@@ -0,0 +1,21 @@
+export function ProjectsCounter () {
+    return (
+        <section 
+            id="projects-counter" 
+            className="p-5 px-4 lg:p-10 lg:px-10 flex justify-center gap-2 lg:gap-20 items-center bg-blackp-500"
+        >
+            <p className="flex flex-col lg:flex-row lg:gap-5 items-center w-50">
+                <span className="text-6xl lg:text-7xl">20</span>
+                <span className="text-center lg:text-left text-sm text-wrap">Projects done</span>
+            </p>
+            <p className="flex flex-col lg:flex-row lg:gap-5 items-center w-55">
+                <span className="text-6xl lg:text-7xl">04</span>
+                <span className="text-center lg:text-left text-sm text-wrap">Years of experience</span>
+            </p>
+            <p className="flex flex-col lg:flex-row lg:gap-5 items-center w-55">
+                <span className="text-6xl lg:text-7xl">15</span>
+                <span className="text-center lg:text-left text-sm text-wrap">Collaborations with brands</span>
+            </p>
+        </section>
+    )
+}
---
+++ src/components/Home/ProjectsCounter/index.js
@@ -0,0 +1,1 @@
+export { ProjectsCounter } from './ProjectsCounter.jsx'
