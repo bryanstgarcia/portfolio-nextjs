bc07db424afc0870aa3da50571fb2252c83205c4
Bryan Garcia <bryan1997st@gmail.com>
2025-08-20 16:50:22 -0500

    Add NavbarHamburguer component: implement a new button for mobile navigation with animated transitions, including associated styles for improved user experience.

---
+++ src/components/shared/NavbarHamburguer/NavbarHamburguer.jsx
@@ -0,0 +1,32 @@
+
+export function NavbarHamburguer({ isOpen, handleClick }) {
+
+	return (
+		<button
+			onClick={handleClick}
+			className='flex flex-col justify-center items-center p-4 lg:hidden'
+		>
+			<span
+				className={`bg-neutral-50 block transition-all origin-[35%] duration-300 ease-out 
+                    h-1 w-6 rounded-sm ${
+						isOpen ? 'rotate-35 translate-y-1' : '-translate-y-0.5'
+					}`
+                }
+			></span>
+			<span
+				className={`bg-neutral-50 block transition-all duration-300 ease-out 
+                    h-1 w-6 rounded-sm my-0.5 ${
+						isOpen ? 'opacity-0' : 'opacity-100'
+					}`
+                }
+			></span>
+			<span
+				className={`bg-neutral-50 block origin-[45%] transition-all duration-300 ease-out 
+                    h-1 w-6 rounded-sm ${
+						isOpen ? '-rotate-35 -translate-y-1' : 'translate-y-0.5'
+					}`
+                }
+			></span>
+		</button>
+	);
+}
---
+++ src/components/shared/NavbarHamburguer/index.js
@@ -0,0 +1,1 @@
+export { NavbarHamburguer } from './NavbarHamburguer.jsx';
