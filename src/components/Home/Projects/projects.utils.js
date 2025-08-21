d3e113d013323c50ffebfcb0770f74a80abd998c
Bryan Garcia <bryan1997st@gmail.com>
2025-08-21 00:11:07 -0500

    Update project repository link in projects.utils.js: add GitHub URL for the devCorner project to enhance accessibility and provide direct access to the codebase.

--- src/components/Home/Projects/projects.utils.js
+++ src/components/Home/Projects/projects.utils.js
@@ -5,7 +5,7 @@ export const projects = [
         description:
             'A blog sharing coding techniques, tools, and tips to help developers learn and grow.',
         path: '/qr-code',
-        repo: '',
+        repo: 'https://github.com/4Practica/devCorner',
         technologies: 'TypeScript | React | Strapi | Docker | NGINX | AWS',
         website: 'https://www.devcorner.top/',
         image: {
